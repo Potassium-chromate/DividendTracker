25c3ddcb0b97724d003a202d588938421228fd70
Potassium-chromate <home7438072@gmail.com>
2025-02-10 23:27:15 +0800

    Update the Log in interface
    Update Login.css

--- DividendTracker/src/Login.jsx
+++ DividendTracker/src/Login.jsx
@@ -6,9 +6,14 @@ function CreateLogIn({setifLogIn}) {
         <>
         <div className="main_page">
             <div className="login_container">
+                <h2>Log in</h2>
                 <input type="text" placeholder="account"/>
-                <input type="password " placeholder="password"/>
-                <button onClick={() => setifLogIn(true)} className="login">log in</button>
+                <input type="password" placeholder="password"/>
+                <div className="login_and_signup">
+                    <button className="signup">Sign up</button>
+                    <button onClick={() => setifLogIn(true)} className="login">Log in</button>
+                </div>
+                
             </div>
         </div>
         </>
