02cc9a4dc867ff6a81b1ba19ece88e474314a876
Eason <home7438072@gmail.com>
2025-02-27 14:05:58 +0800

    refactor: Use environment variables for SQL connection
    Updated `config.py` to use environment variables for SQL connection details,
    ensuring better compatibility with Docker. Also updated `.env` and
    `docker-compose.yml` to reflect these changes.
    
    Changes to be committed:
    	1. modified:   DividendTracker/.env
    	2. modified:   DividendTracker/backend/config.py
    	3. modified:   DividendTracker/docker-compose.yml

--- DividendTracker/backend/config.py
+++ DividendTracker/backend/config.py
@@ -2,12 +2,11 @@ import os
 
 class Config:
     def __init__(self):
-        self.Username = os.getenv("DB_USER", "SA")            # fallback to "SA"
-        self.Password = os.getenv("DB_PASSWORD", "password")  # fallback password
-        self.Server   = os.getenv("DB_SERVER", "localhost")   # fallback host
-        self.Data_Base = os.getenv("DB_NAME", "Data_Base")    # fallback DB name
-        self.Driver   = os.getenv("DB_DRIVER", "ODBC Driver 17 for SQL Server")
+        self.Username = os.getenv("DB_USER")            # fallback to "SA"
+        self.Password = os.getenv("DB_PASSWORD")  # fallback password
+        self.Server   = os.getenv("DB_SERVER")   # fallback host
+        self.Data_Base = os.getenv("DB_NAME")    # fallback DB name
+        self.Driver   = os.getenv("DB_DRIVER")
         
     def config(self):
-        return f"mssql+pyodbc://{self.Username}:{self.Password}@{self.Server}/{self.Data_Base}?driver={self.Driver}"
-
+    	return f"mssql+pyodbc://{self.Username}:{self.Password}@{self.Server}/{self.Data_Base}?driver={self.Driver}"
